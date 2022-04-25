27454b732305fec6a330c9260bb3b404c47dd253
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2022-04-25 09:58:35 +0700

    implemented game state checker (for now, without 'draw' state) code is not tested at all :(

--- main.asm
+++ main.asm
@@ -3,7 +3,7 @@ IOReg:	# Gives the address 0xf3 the symbolic name IOReg
 		asect	0xf0
 stack:	# Gives the address 0xf0 the symbolic name stack
 
-		asect 	0x00
+		asect 	0x20
 table:  dc 0,1,2     # Horizontal lines
 		dc 4,5,6
 		dc 8,9,10
@@ -23,6 +23,9 @@ game: dc 0b00000000
 win:  dc 0b01000000
 lose: dc 0b10000000
 draw: dc 0b11000000
+
+crossWins:  dc 0b00101010
+noughtWins: dc 0b00010101
 
 
 init:	
@@ -46,22 +49,19 @@ main:
 		jsr humanPlays  # Put a cross 
 						# and check if human wins.
 						#
-						# IOReg[0-1]   := cross
-						# IOReg[2-5]   := cellAddr (r2)
-						# IOReg[6-7]   := state
 						#   cellData   := cross
 						#         r2   := state
 
 		if
-			tst r2
-		is  z
-			break
+			tst r2      # If state is win, lose or draw
+		is  nz
+			break		# We finish the game
 		fi
 		
 
-		# jsr robotPlays  # Find suitable cell for a nought
+		# jsr robotPlays  # Find suitable cell for a nought 
 						# And check if robot wins.
-						# 
+						# not implemented yet.
 		
 		
 		
@@ -101,6 +101,74 @@ getCellAddr:
 
 	# Result is stored in r3
 	rts
+
+
+getGameState:
+	stsp r0
+	stsp r1
+	stsp r3
+
+	ldi r0, table
+	ldi r3, 8
+
+	while 
+		dec r3
+	stays nz
+		ldi r2, 0x00
+		ld  r1, r0
+
+		shl r1
+		shl r1
+		shl r1
+		shl r1
+
+		or  r1, r2   # Bits 4-5 of r2 is the 1st cell
+
+		inc r0
+		ld  r1, r0
+
+		shl  r1
+		shl  r1
+
+		or  r1, r2   # Bits 2-3 of r2 is the 2nd cell
+
+		inc r0
+		ld  r1, r0
+
+		or  r1, r2   # Bits 0-1 of r2 is the 3rd cell
+
+		inc r0
+
+		# Win, if there are three crosses in a row
+		# Lose, if there are three noughts in a row
+		# Draw, when all the cells are busy
+		# ...and two previous statements are false
+
+		if
+			ldi r1, crossWins
+			cmp r1, r2
+		is eq
+			ldi r2, win
+			break
+		else
+			if
+				ldi r1, noughtWins
+				cmp r1, r2
+			is eq
+				ldi r2, lose
+				break
+			else
+				ldi r2, game
+			fi
+		fi
+	wend
+
+	ldsp r3
+	ldsp r1
+	ldsp r0
+
+	rts
+	# Game state is stored in r2
 	
 	
 changeIO:
@@ -112,10 +180,6 @@ changeIO:
 	# Symbol ID is stored in bits 0-1 of IO data
 	# Ours is in r1
 
-
-	st   r3, r1     # cellData := symbol
-
-
 	shl  r3
 	shl  r3
 
@@ -123,8 +187,7 @@ changeIO:
 	or   r2, r3
 
 	# Now r3 contains updated IO data
-
-	st   r0, r3     # r0 is IOReg
+	st   r0, r3
 	
 	rts
 	
@@ -132,12 +195,12 @@ changeIO:
 humanPlays:
 	# cellAddr is stored in r3
 
-	ldi  r2, game    # game state checker is not yet implemented
-	ldi  r1, cross
+	ldi  r1, cross  
+	st   r3, r1        # cellData := symbol
 
-	jsr changeIO
+	jsr  getGameState  # r2 := gameState
+	jsr  changeIO
 
 	rts
-
 
 end
