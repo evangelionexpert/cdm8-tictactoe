24546c9d86244352eb12af0016eae38d8bedae9c
Pavel Smolyakov <pavel.smolyakov.83@gmail.com>
2022-05-11 07:21:30 +0700

    some improvements

--- main_with_extensions.asm
+++ main_with_extensions.asm
@@ -1,9 +1,9 @@
 		asect	0xf3
 IOReg:	# Main IO address is 0xf3
 		asect   0xf2
-IOExt:  # Additional IO adress (for extensions) is 0xf2
+IOExt:  # IO address for extensions is 0xf2
 		asect	0xf0
-stack:	# Address we'll put to stack pointer is 0xf0
+stack:	# Address we'll store to stack pointer is 0xf0
 		asect   0xe0
 celCnt: # And amount of busy cells will be stored in address 0xe0
         # It's an optimization for game state checker
@@ -18,12 +18,12 @@ table:  dc 0,1,2     # Horizontal lines
 		dc 2,6,10
 		dc 0,5,10    # Diagonal lines
 		dc 8,5,2
-# Table will be stored in bytes 0-10 of RAM
+# Table contents will be stored in bytes 0-10 of RAM
 
 
 defendingTable:  dc 5         # Center cell (minus 4 variants max)
-                 dc 0,2,8,10  # Cells at edges (minus 3 variants max)
-				 dc 1,4,6,9   # All others (minus 2 variants max)
+                 dc 0,2,10,8  # Cells at edges (minus 3 variants max)
+				 dc 1,9,6,4   # All others (minus 2 variants max)
 
 # Game states:
 game: dc 0b00000000
@@ -37,65 +37,61 @@ space:  dc 0
 cross:  dc 1
 nought: dc 2
 
+
 # Turns:
-nothing    :   dc 0
 correctTurn:   dc 1
 incorrectTurn: dc 2
+
 
 init:	
 	ldi	 r0, stack
 	stsp r0
 	
 mainLoop:
-	do
-		jsr readIO      #  cell -> r3
-		ld	r3, r1      # *cell -> r1
-		
-		if 	       
-			tst r1
-		is	nz
-          	# If cell is not empty,
-			# write that turn is not correct
-			# and wait for new button press
-
-			ldi r1, incorrectTurn
-			jsr storeExt
-
-			continue 
-		else 
-			# Write that turn is correct
-
-			ldi r1, correctTurn
-			jsr storeExt
-		fi
-
-		
-		jsr humanPlays     # Put a cross 
-						   # and check if human wins.
-						   #    *cell   := cross
-						   #       r2   := state
-
-		if
-			tst r2         # If game continues, ...
-		is  z
-			jsr robotPlays # find suitable cell for a nought 
-						   # and check if robot wins.
-						   #  *freeCell  := nought
-						   #         r2  := state
-		fi		
+	jsr readIO          #  cell -> r3
+	ld	r3, r1          # *cell -> r1
 	
+	if 	       
+		tst r1
+	is	nz
+		ldi r1, incorrectTurn
+		jsr storeExt    # Write that turn is not correct
 
-		tst	r2
-	until hs 	# Infinite loop
+		br  mainLoop    # Go to the beginning of loop
+	else 
+		ldi r1, correctTurn
+		jsr storeExt	# Write that turn is correct
+	fi
+
 	
+	jsr humanPlays      # Put a cross 
+						# and check if human wins.
+						#    *cell   := cross
+						#       r2   := state
+
+	if
+		tst r2          # If game continues, ...
+	is  z
+		jsr robotPlays  # find suitable cell for a nought 
+						# and check if robot wins.
+						#  *freeCell  := nought
+						#         r2  := state
+	fi		
+
+	br  mainLoop 	    # Infinite loop
 	halt
 
+
 clean:
+	# We call this subroutine while waiting for input
+	# so we can mess up everything but not r3
+
 	push r3
 
 	ldi r3, 0
 	ldi r1, 10
 	ldi r0, table
+
 	while 
 		dec r1
 	stays nz
@@ -107,12 +103,12 @@ clean:
 		shl r2
 		
 		ldi r3, IOReg
-		st r3, r2
+		st  r3, r2
 		ldi r3, 0				 
 	wend
 	
-	ldi r1, celCnt
 	clr r2
+	ldi r1, celCnt
 	st  r1, r2
 	
 	pop r3
@@ -121,20 +117,27 @@ clean:
 
 
 incCells:
+	# Load global counter from dedicated address
 	ldi r1, celCnt
 	ld  r1, r2
 
+	# Increment it
 	inc r2
+
+	# And store in the same place
 	st  r1, r2
 	rts
 
 
 storeExt:
+	# r1 contains constant
+
 	ldi r2, IOExt
 	ldc r1, r1
 	st  r2, r1
 	rts
  
+
 readIO:
 	do
 		ldi r0, IOReg
@@ -148,7 +151,7 @@ readIO:
 		fi
 		
 		tst	r3
-	until mi		 # Wait for bit 7 raise
+	until mi		   # Wait for bit 7 raise
 	
 	
 	ldi r0, 0b00001111
@@ -222,16 +225,20 @@ getGameState:
 	
 	
 	if 
-		tst r2
+		tst r2				  # If we didn't lose or win
 	is  z, and                # Check for "draw" state
 		ldi r1, celCnt
 		ld  r1, r1
 
 		ldi r3, 9
-		cmp r1, r3
-	is  eq
-		ldi r2, 3
+		cmp r1, r3			  # If global counter is equal to 9
+	is  eq                   
+		ldi r2, 3             # Then the state is "draw"
 	fi
+
+	ldi  r1, game
+	add  r1, r2
+	ldc  r2, r2 
 	
 	pop r3
 	pop r1
@@ -241,8 +248,8 @@ getGameState:
 
 	
 storeIO:
-	# cellAddr should be stored in bits 2-5 of IO data
-	# Ours is in r3
+	# Cell address should be stored in bits 2-5 of IO data
+	# Ours is in bits 0-3 of r3
 	
 	# Game state is stored in bits 6-7 of IO data
 	# Ours is in r2
@@ -256,27 +263,23 @@ storeIO:
 	shl  r3
 	
 	or   r1, r3
-	
-	ldi  r1, game
-	add  r1, r2
-	ldc  r2, r2 
-	
 	or   r2, r3	
 
 	# Now r3 contains updated IO data
 	ldi  r0, IOReg
 	st   r0, r3
+
 	rts
 
 
 humanPlays:
 	jsr incCells
-	# cellAddr is stored in r3
+	# Cell address is stored in r3
 
 	ldi  r1, cross
 	ldc  r1, r1
 	  
-	st   r3, r1        # cellData := symbol
+	st   r3, r1        # *cell := symbol
 
 	jsr  getGameState  # r2 := gameState
 	jsr  storeIO
@@ -308,7 +311,7 @@ geniusAI:
 	
 robotPlays:
 	jsr incCells
-	jsr geniusAI 	   # free cell -> r3
+	jsr geniusAI 	  # free cell -> r3
 	
 	ldi r1, nought
 	ldc r1, r1
